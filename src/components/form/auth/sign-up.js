3e3698197b0a5f111fb64bb56477ff5b06153503
KosM <kosukemorimoto1@gmail.com>
2021-08-01 19:58:41 +0900

    refactored some code

--- src/components/form/auth/sign-up.js
+++ src/components/form/auth/sign-up.js
@@ -49,79 +49,50 @@ const SignUp = ({ toggleAuth, variants: { form, item } }) => {
   });
 
   return (
-    <form
+    <motion.form
       onSubmit={handleSubmit(onSubmit)}
-      className="max-w-90 mx-auto sm:max-w-lg bg-black px-8 py-10 sm:px-12 sm:py-16 bg-opacity-75 rounded-lg"
+      initial="hidden"
+      animate="visible"
+      variants={form}
     >
-      <h2 className="text-gray-200 text-xl xs:text-2xl font-semibold">
-        Sign Up
-      </h2>
-      <p className="text-red-600 text-sm font-semibold text-center mt-4 xs:mt-5 mb-2 xs:mb-4">
-        Pay attention: this is not the original Netflix sign in. Don't insert
-        your real credentials here!!
-      </p>
-      <motion.div initial="hidden" animate="visible" variants={form}>
-        <motion.div variants={item} className="mb-3 xs:mb-4">
-          <AuthInput
-            id="username"
-            placeholder="Your name"
-            errors={errors}
-            registration={usernameRegistration}
-          />
-        </motion.div>
-        <motion.div variants={item} className="mb-3 xs:mb-4">
-          <AuthInput
-            id="email"
-            placeholder="E-mail"
-            errors={errors}
-            registration={emailRegistration}
-          />
-        </motion.div>
-        <motion.div variants={item} className="mb-3 xs:mb-4">
-          <AuthInput
-            type="password"
-            id="password"
-            placeholder="Password"
-            errors={errors}
-            registration={passwordRegistration}
-          />
-        </motion.div>
-        <motion.div variants={item} className="mb-8">
-          <AuthInput
-            type="password"
-            id="passwordConfirmation"
-            placeholder="Repeat your password"
-            errors={errors}
-            registration={passwordConfirmationRegistration}
-          />
-        </motion.div>
-        <motion.div variants={item}>
-          <AuthButton type="submit" text="Sign in" color="primary" />
-        </motion.div>
+      <motion.div variants={item} className="mb-3 xs:mb-4">
+        <AuthInput
+          id="username"
+          placeholder="Your name"
+          errors={errors}
+          registration={usernameRegistration}
+        />
       </motion.div>
-      <hr className="mt-6 w-11/12 mx-auto text-gray-500" />
-      <p
-        className="
-          mt-6
-          text-sm md:text-base text-center
-        text-gray-500
-        "
-      >
-        Do you already have an account?
-        <span
-          className="
-            ml-2 inline-block
-          text-red-600 font-bold
-            cursor-pointer
-            duration-300
-          hover:text-red-700 hover:transform-gpu hover:scale-95
-          "
-          onClick={() => toggleAuth((state) => !state)}
-        >
-          Sign In
-        </span>
-      </p>
-    </form>
+      <motion.div variants={item} className="mb-3 xs:mb-4">
+        <AuthInput
+          id="email"
+          placeholder="E-mail"
+          errors={errors}
+          registration={emailRegistration}
+        />
+      </motion.div>
+      <motion.div variants={item} className="mb-3 xs:mb-4">
+        <AuthInput
+          type="password"
+          id="password"
+          placeholder="Password"
+          errors={errors}
+          registration={passwordRegistration}
+        />
+      </motion.div>
+      <motion.div variants={item} className="mb-8">
+        <AuthInput
+          type="password"
+          id="passwordConfirmation"
+          placeholder="Repeat your password"
+          errors={errors}
+          registration={passwordConfirmationRegistration}
+        />
+      </motion.div>
+      <motion.div variants={item}>
+        <AuthButton type="submit" text="Sign in" color="primary" />
+      </motion.div>
+    </motion.form>
   );
 };
 
--- src/pages/login.js
+++ src/pages/login.js
@@ -70,16 +70,50 @@ export default function LoginPage() {
             />
           </motion.div>
           <motion.div
-            className="z-20"
+            className="
+                max-w-90 mx-auto sm:max-w-lg bg-black px-8 py-10 sm:px-12 sm:py-16 bg-opacity-70 rounded-lg z-20
+              "
             initial="hidden"
             animate="visible"
             variants={variants.container}
           >
+            <h2 className="text-gray-200 text-xl xs:text-2xl font-semibold">
+              {isSignedUp ? 'Sign In' : 'Sign Up'}
+            </h2>
+            <p className="text-red-600 text-sm font-semibold text-center mt-4 xs:mt-5 mb-2 xs:mb-4">
+              Pay attention: this is not the original Netflix sign in. Don't
+              insert your real credentials here!!
+            </p>
             {isSignedUp ? (
-              <SignIn toggleAuth={setIsSignedUp} variants={variants} />
+              <SignIn variants={variants} />
             ) : (
-              <SignUp toggleAuth={setIsSignedUp} variants={variants} />
+              <SignUp variants={variants} />
             )}
+            <hr className="mt-6 w-11/12 mx-auto text-gray-500" />
+            <p
+              className="
+                mt-6
+                text-sm md:text-base text-center
+              text-gray-500
+              "
+            >
+              {isSignedUp
+                ? "Haven't you registered yet?"
+                : 'Do you already have an account?'}
+
+              <span
+                className="
+                  ml-2 inline-block
+                  text-red-600 font-bold
+                  cursor-pointer
+                  duration-300
+                hover:text-red-700 hover:transform-gpu hover:scale-95
+                "
+                onClick={() => setIsSignedUp((state) => !state)}
+              >
+                {isSignedUp ? 'Sign Up' : 'Sign In'}
+              </span>
+            </p>
           </motion.div>
         </div>
       </div>
